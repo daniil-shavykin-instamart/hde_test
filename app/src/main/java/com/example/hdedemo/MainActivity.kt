3fbfb9f421762fc1e7c2a852af986e949f0c50c7
Daniil Shavykin <daniil.shavykin@sbermarket.ru>
2022-04-04 17:41:58 +0300

    fix

--- app/src/main/java/com/example/hdedemo/MainActivity.kt
+++ app/src/main/java/com/example/hdedemo/MainActivity.kt
@@ -16,9 +16,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.hdedemo.ui.theme.HDEDemoTheme
 
-const val CHAT_URL =
-    "https://ashunicorn.helpdeskeddy.com/ru/omnichannel/chat?id=6270f045-2efd-482d-bcfb-0dc98c048248"
-//    "https://google.com"
+// Put a valid HDE url instead
+const val CHAT_URL = "https://google.com"
 
 class MainActivity : ComponentActivity() {
     //    private lateinit var webViewClient: WebViewClient

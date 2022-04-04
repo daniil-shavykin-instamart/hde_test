fafaa9c10163ea0e9fd90167e4f70937cb0a77e0
Daniil Shavykin <daniil.shavykin@sbermarket.ru>
2022-04-04 17:39:06 +0300

    ашч

--- app/src/main/java/com/example/hdedemo/MainActivity.kt
+++ app/src/main/java/com/example/hdedemo/MainActivity.kt
@@ -49,16 +49,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    HDEDemoTheme {
-        Greeting("Android")
-    }
-}
